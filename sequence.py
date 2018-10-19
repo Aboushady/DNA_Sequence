b6d6ad6763e8c42305191e75bdcaf8e513c4ec26
verbonus <mostafaaboshadee@yahoo.com>
2018-10-19 17:04:37 +0200

    Sequence class, next 5 tasks of the project, and start.py

--- sequence.py
+++ sequence.py
@@ -1,74 +1,109 @@
 class Sequence:
-    def __init__(self, seq_string, type):
+    def __init__(self, seq_string, gene_type):
+        # Private fields.
         self._seq_str = seq_string
         self._check_dna_seq = True
-        self._type = type
+        self._type = gene_type
 
+    # Getting the number of bases(characters), in a sequence string.
     def get_dna_bases(self):
         number_of_bases = len(self._seq_str)
         return number_of_bases
 
+    # Checking whether the sequence string is valid,
+    # that is if it contains all the valid bases in it ['A', 'T', 'G', 'C'] and 'U' in case it was an RNA sequence.
     def is_valid(self):
-        dna_bases = ['a', 't', 'c', 'g']
-        if self._type.lower() == 'rna':
-            dna_bases.append('u')
-            dna_bases= [e for e in dna_bases if e not in ['T', 't']]
+        dna_bases = ['a', 't', 'c', 'g']                              # Valid bases.
+        if self._type.lower() == 'rna':                               # Checks if the string is RNA seq.
+            dna_bases.append('u')                                     # Append 'u' to the valid base.
+            dna_bases= [e for e in dna_bases if e not in ['T', 't']]  # Creates a new list without the t' base in it.
         for c in dna_bases:
-            if c not in list(self._seq_str.lower()):
-                self._check_dna_seq = False
+            if c not in list(self._seq_str.lower()):                  # if one of the bases in the valid list isn't in
+                self._check_dna_seq = False                           # the string, return 0.
                 return 0
 
-        return 1
+        return 1                                                      # else return 1.
 
+    # Checks the equality of objects.
     def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            return self._seq_str.lower() == other.get_seq_str().lower()
-        return False
+        if isinstance(other, self.__class__):                               # If the two objects are instances of the
+            return self._seq_str.lower() == other.get_seq_str().lower()     # same class. return whether their,
+        return False                                                        # seq strings are equal. else return False.
 
+    # Checks for inequality of objects. (same behaviour as the __eq__(self):)
     def __ne__(self, other):
         if isinstance(other, self.__class__):
             return self._seq_str.lower() != other.get_seq_str().lower()
         return False
 
+    # A get method to access self._seq_str private field, from outside of the class.
     def get_seq_str(self):
         seq_tr_temp = self._seq_str
         return seq_tr_temp
 
+    # Gets the complement of each gene base
+    # Return the complement string as the seq string of another object.
     def dna_complement(self):
+        # Dictionary of each base(Dict's Key) and it's complement(Dict's value).
         dna_dic = {'A': ['T', 'U'], 'T': 'A', 'C': 'G', 'G': 'C', 'U': 'A', 'u': 'a', 't': 'a', 'a': ['t', 'u'], 'c': 'g', 'g': 'c'}
+
+        # Convert the seq string into a list.
         seq_ls = list(self._seq_str)
+
+        # Looping through seq_ls[] list.
         for i in range(len(seq_ls)):
+            # Looping through the Dict.
             for key, value in dna_dic.items():
-                if key == seq_ls[i]:
-                    if self._type.lower() == 'rna' and key.lower() == 'a':
-                        seq_ls[i] = value[1]
+                if key == seq_ls[i]:                                        # If the Key equals the list's element.
+                    if self._type.lower() == 'rna' and key.lower() == 'a':  # Checks for 'a', cause it's the only key,
+                        seq_ls[i] = value[1]                                # with a list of two values.
                         break
-                    elif key.lower() == 'a':
-                        seq_ls[i] = value[0]
-                        break
-                    else:
+                    elif key.lower() == 'a':                                # If the string was a DNA string assign the
+                        seq_ls[i] = value[0]                                # assign the first value of the list,
+                        break                                               # else assign the second value, like the
+                    else:                                                   # above if statement.
                         seq_ls[i] = value
                         break
                     continue
-        complement_dna_str = ''.join(seq_ls)
-        seq_obj = Sequence(complement_dna_str, self._type)
+        complement_dna_str = ''.join(seq_ls)                                # Convert seq_ls[] list back to a string.
+        seq_obj = Sequence(complement_dna_str, self._type)                  # Create a new object with complement_dna_str.
         return seq_obj
 
+    # Checks for the first non-match bases between two seq strings.
+    # Takes an object as a parameter.
+    # Returns : 1- the non-zero index of the first non-matching indexes.(if there were any).
+    #           2- -1 if they were completely matching strings.
+    #           3- Raises an exception in case the two strings weren't of the same length.
     def find_non_matching_bases(self, other_obj):
-        this_ls, other_ls = list(self._seq_str), list(other_obj.get_seq_str())
-        if len(this_ls) == len(other_ls):
+        this_ls, other_ls = list(self._seq_str), list(other_obj.get_seq_str())  # Converting this and the other object's
+        if len(this_ls) == len(other_ls):                                       # strings to lists.
             for i in range(len(this_ls)):
-                if this_ls[i].lower() != other_ls[i].lower():
-                    return i
+                if this_ls[i].lower() != other_ls[i].lower():                   # Checks the equality of each element,
+                    return i                                                    # of the two lists, then returns the index.
         else:
-            raise Exception('cannot compare sequences of different lengths')
-        return -1
+            raise Exception('cannot compare sequences of different lengths')    # Raise an exception
+        return -1                                                               # other wise returns -1.
 
+    # Compares two objects' seq strings, for swap mutations.
+    # Parameters : 1- gene_ls_01 (List of objects).
+    #              2- gene_ls_02 (List of objects).
+    # Returns : 1- swap_len_ls_temp (List of number swap mutations/gene)
     def compare_dna_seq(self, genes_ls_01, genes_ls_02):
         swap_ls = []
         for i in range(len(genes_ls_01)):
-            seq_str_temp = genes_ls_01[i].get_seq_str()
+            seq_str_temp = genes_ls_01[i].get_seq_str()                 # gets the seq sting of each object in the list.
             seq_str_temp_1 = genes_ls_02[i].get_seq_str()
+
+            # Creates a new list with indexes for where the swap mutations happened in each gene.
             swap_ls.append([j for j in range(len(seq_str_temp)) if seq_str_temp[j].lower() != seq_str_temp_1[j].lower()])
-        swap_len_ls_temp = [len(swap_ls[i]) for i in range(len(swap_ls))]
+        swap_len_ls_temp = [len(swap_ls[i]) for i in range(len(swap_ls))]  # Gets the number of swap mutations in each gene.
         return swap_len_ls_temp
+
+    # Split the string at the occurrence of 'AAAAAAAAAATTTTTTTTTT',
+    def get_genes_from_seq(self):
+        genes_ls = self._seq_str.split('AAAAAAAAAATTTTTTTTTT')
+        # returns a list of genes.
+        genes_obj_ls_temp = []
+        for i in range(len(genes_ls)):
+            genes_obj_ls_temp.append(Sequence(genes_ls[i], self._type))  # Creates an object with each gene string in genes_ls[]
+        return genes_obj_ls_temp
--- start.py
+++ start.py
@@ -1,6 +1,7 @@
 from sequence import Sequence
 import matplotlib.pyplot as plt
 
+
 def main():
     file_name = 'genome_01.dat'
     file_name_1 = 'genome_02.dat'
@@ -8,58 +9,54 @@ def main():
     seq_type = input('Please write \'DNA\' if the sequence is a DNA sequence\n'
                      'and \'RNA\' if the it\'s an RNA sequence')
 
-    #Object created from sequence string in genome_02.dat file.
+    # Object created from sequence string in genome_02.dat file.
     genome_02_obj = read_genome(file_name_1, seq_type)
 
-    #Two objects  created from sequence string in genome_01.dat file, equals each other but not the same object.
+    # Two objects  created from sequence string in genome_01.dat file, equals each other but not the same object.
     genome_01_obj = read_genome(file_name, seq_type)
     genome_01_obj_1 = read_genome(file_name, seq_type)
 
-    #Applying all the testing required.
+    # Applying all the testing required.
     test_functionality(genome_01_obj, genome_01_obj_1, seq_type)
 
+    # Getting the genes from the given sequence, for both objects, returns a list of objects.
+    genes_obj_ls_01 = genome_01_obj.get_genes_from_seq()
+    genes_obj_ls_02 = genome_02_obj.get_genes_from_seq()
 
-
-    genes_obj_ls_01 = get_genes_from_seq(genome_01_obj.get_seq_str(), seq_type)
-    genes_obj_ls_02 = get_genes_from_seq(genome_02_obj.get_seq_str(), seq_type)
-
+    # Getting the number of swap mutations, returns a list of number of swap mutation/gene
     swap_len_ls = genome_02_obj.compare_dna_seq(genes_obj_ls_01, genes_obj_ls_02)
 
     print('This is the first Gene : ' + genes_obj_ls_01[0].get_seq_str())
+
+    # Plotting the genes length in a histogram plot, retunrs a list of genes length.
     genes_len_ls = plot_gene_lengths(genes_obj_ls_01)
 
+    # Plotting swap mutations/gene against genes length.
     plot_swap_mutations(genes_len_ls, swap_len_ls)
 
 
 def read_genome(file_name, seq_type_temp):
-    f = open(file_name, 'r')
+    f = open(file_name, 'r')                                # Opening the ASCII file for read.
     count = 0
     seq_str_temp = ''
-    for line in f:
-        if count == 0:
+    for line in f:                                          # Loops through the file.
+        if count == 0:                                      # IF this is the first line, skip.(First line has only headers)
             count += 1
             continue
         else:
-            seq_str_temp = line
+            seq_str_temp = line                             # If not read the line and save it to seq_str_temp
     f.close()
-    seq_obj_temp = Sequence(seq_str_temp, seq_type_temp)
+    seq_obj_temp = Sequence(seq_str_temp, seq_type_temp)    # Creates a new object with that string.
     print('The total number of bases is:' + str(len(seq_str_temp)))
     return seq_obj_temp
 
 
-def get_genes_from_seq(seq_str, seq_type_temp):
-    genes_ls = seq_str.split('AAAAAAAAAATTTTTTTTTT')
-    genes_obj_ls_temp = []
-    for i in range(len(genes_ls)):
-        genes_obj_ls_temp.append(Sequence(genes_ls[i], seq_type_temp))
-    return genes_obj_ls_temp
-
-
 def plot_gene_lengths(genes_obj_ls_temp):
     genes_len_ls_temp = []
-    for i in genes_obj_ls_temp:
-        genes_len_ls_temp.append(len(i.get_seq_str()))
-    fig = plt.figure()
+    for i in genes_obj_ls_temp:                             # Loops through the list of objects.
+        genes_len_ls_temp.append(len(i.get_seq_str()))      # Getting the gene string of each object, then take it's len(),
+                                                            # and save it in genes_len_ls[]
+    fig = plt.figure()                                      # Plotting the histogram plot.
     plt.title('Genes Histogram')
     plt.xlabel('Genes Lengths')
     plt.ylabel('Genes Density')
@@ -69,10 +66,11 @@ def plot_gene_lengths(genes_obj_ls_temp):
 
     plt.show()
     fig.savefig('genes_hist.png')
-    return genes_len_ls_temp
+    return genes_len_ls_temp                                # Returning the list of gene lengths.
 
 
-def plot_swap_mutations(genes_len_ls_temp, swap_len_ls_temp):
+def plot_swap_mutations(genes_len_ls_temp, swap_len_ls_temp):           # Plotting the number of swap mutations/genes,
+                                                                        #  against gene length.
     plt.title('Number of swap mutations/gene against gene length')
     plt.xlabel('Genes Lengths')
     plt.ylabel('Number of swap mutations')
@@ -82,32 +80,30 @@ def plot_swap_mutations(genes_len_ls_temp, swap_len_ls_temp):
 
 
 def test_functionality(seq_obj_01_temp, seq_obj_02_temp, seq_type_temp):
-    #Test task #3, is_valid(self).
+    # Test task # 3, is_valid(self).
     if seq_type_temp.lower() == 'dna' :
         assert seq_obj_01_temp.is_valid()
 
+    #  Test task# 4, __eq__(self, other).
+    assert (seq_obj_01_temp == seq_obj_02_temp)                 # Equal objects but not the same.
 
+    #  Test task # 5, dna_complement(self).
+    test_obj = Sequence('ACGTTCGATCG', 'DNA')                    # Creating an object
+    test_complement_obj = test_obj.dna_complement()              # Getting the complement of sequence string of test_obj object.
+    assert (test_complement_obj.get_seq_str() == 'TGCAAGCTAGC')  # Comparing the two sequences strings.
 
-    #Test task#4, __eq__(self, other).
-    assert (seq_obj_01_temp == seq_obj_02_temp)                 #Equal objects but not the same.
-
-    #Test task #5, dna_complement(self).
-    test_obj = Sequence('ACGTTCGATCG', 'DNA')                    #Creating an object
-    test_complement_obj = test_obj.dna_complement()              #Getting the complement of sequence string of test_obj object.
-    assert (test_complement_obj.get_seq_str() == 'TGCAAGCTAGC')  #Comparing the two sequences strings.
-
-    #Test task#6, find_non_matching_bases(self, other_obj):
-    non_match = test_obj.find_non_matching_bases(test_complement_obj)        #Each object has the complement string of the other,
+    #  Test task# 6, find_non_matching_bases(self, other_obj):
+    non_match = test_obj.find_non_matching_bases(test_complement_obj)        # Each object has the complement string of the other,
     assert (non_match == 0)                                                  # should return the first non-zero index.
 
-    #Test task#11,  __eq__(self, other).
-    test_lower_case_obj = Sequence(seq_obj_01_temp.get_seq_str().lower(), seq_type_temp)    #test_lower_case_obj. has the same sequence string
-                                                                             # as seq_obj_01, but in lower case.
+    #  Test task# 11,  __eq__(self, other).
+    test_lower_case_obj = Sequence(seq_obj_01_temp.get_seq_str().lower(), seq_type_temp)    # test_lower_case_obj. has the same sequence string
+                                                                                            # as seq_obj_01, but in lower case.
 
-    assert(seq_obj_01_temp == test_lower_case_obj)                           #Testing the equality with different cases.
+    assert(seq_obj_01_temp == test_lower_case_obj)                           # Testing the equality with different cases.
 
-    #Test task#13,  __eq__(self, other), and dna_complement(self),
-    #when the user specify that the sequence is RNA.
+    #  Test task# 13,  __eq__(self, other), and dna_complement(self),
+    #  when the user specify that the sequence is RNA.
     rna_seq_str = seq_obj_01_temp.get_seq_str().replace('T', 'U')
     rna_obj = Sequence(rna_seq_str, seq_type_temp)
     if seq_type_temp.lower() == 'rna':
